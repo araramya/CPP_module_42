818908544ca8cf634713f43ae9819f23df8bec3a
Arsen Aramyan <araramya@c2r1s5.42yerevan.am>
2022-05-08 15:58:42 +0400

    Done

--- cpp_new_modules/cpp01/ex03/Weapon.hpp
+++ cpp_new_modules/cpp01/ex03/Weapon.hpp
@@ -12,7 +12,7 @@ class Weapon
         Weapon(std::string type);
         ~Weapon();
         const std::string&  getType();
-        void               setType(std::string type);
+        void                setType(std::string type);
 };
 
 #endif
