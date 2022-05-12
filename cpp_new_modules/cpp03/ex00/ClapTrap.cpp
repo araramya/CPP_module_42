6d1d7788805162e2e6c1494c5e12b73704d0be01
Arsen Aramyan <araramya@c2r18s4.42yerevan.am>
2022-05-12 22:12:07 +0400

    gnam tun sharunakem(((

---
+++ cpp_new_modules/cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,43 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+    std::cout << "Defaoul Constructor Called" << std::endl;
+    name = "Default_name";
+    hit_points = 10;
+    energy_points = 10;
+    attack_demage = 0;
+}
+
+ClapTrap::ClapTrap(const std::string& name)
+{
+    std::cout << "Constructor for " << name << " called" << std::endl;
+    this->name = name;
+    hit_points = 10;
+    energy_points = 10;
+    attack_demage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+    std::cout << "Copy Constructor Called" << std::endl;
+    this->name = other.name;
+    this->hit_points = other.hit_points;
+    this->attack_demage = other.attack_demage;
+    this->energy_points = other.energy_points;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &other)
+{
+    if(this == &other)
+        return *this;
+    this->name = other.name;
+    this->hit_points = other.hit_points;
+    this->attack_demage =other.attack_demage;
+    this->energy_points = other.energy_points;
+}
+
+void ClapTrap::attack(const std::string&)
+{
+
+}
